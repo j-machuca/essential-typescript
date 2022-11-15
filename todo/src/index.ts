6b0a41a02567f27b1f08429c160e86c33f4d8004
Jorge Machuca <jmachuca06@gmail.com>
2022-11-15 09:17:39 -0300

    typescript basics

--- todo/src/index.ts
+++ todo/src/index.ts
@@ -11,28 +11,84 @@ let todos: TodoItem[] = [
 ]
 
 let collection: TodoCollection = new TodoCollection("Adam", todos);
+let showCompleted = true;
 
 function displayTodoList(): void {
     console.log(`${collection.userName}'s Todo List (${collection.getItemCounts().incomplete} items to do)`);
-    collection.getTodoItems(true).forEach(item => item.printDetails());
+    collection.getTodoItems(showCompleted).forEach(item => item.printDetails());
 }
 
 enum Commands {
+    Add = "Add New Task",
+    Complete = "Complete Task",
+    Toggle = "Show/Hide Completed",
+    Purge = "Remove Completed Tasks",
     Quit = "Quit"
 }
 
-function promptUser(): void {
+function promptAdd(): void {
+    console.clear();
+    inquirer.prompt({
+        type: "input",
+        name: "add",
+        message: "Enter task:"
+    }).then(answers => {
+        if (answers['add'] !== "") {
+            collection.addTodo(answers["add"])
+        }
+        promptUser();
+    })
+}
+
+function promptComplete(): void {
     console.clear()
-    displayTodoList()
+    inquirer.prompt({
+        type: 'checkbox',
+        name: 'complete',
+        message: 'Mark Tasks Complete',
+        choices: collection.getTodoItems(showCompleted).map(
+            item => ({ name: item.task, value: item.id, checked: item.complete }))
+    }).then(answers => {
+        let completedTasks = answers["complete"] as
+            number[];
+        collection.getTodoItems(true).forEach(item =>
+            collection.markComplete(item.id,
+                completedTasks.find(id => id ===
+                    item.id) != undefined));
+        promptUser();
+    })
+}
+
+function promptUser(): void {
+    console.clear();
+    displayTodoList();
     inquirer.prompt({
         type: "list",
         name: "command",
         message: "Choose option",
         choices: Object.values(Commands)
     }).then(answers => {
-        if (answers["command"] !== Commands.Quit) {
-            promptUser();
+        switch (answers['command']) {
+            case Commands.Toggle:
+                showCompleted = !showCompleted;
+                promptUser();
+                break;
+            case Commands.Add:
+                promptAdd();
+                break;
+            case Commands.Complete:
+                if (collection.getItemCounts().incomplete > 0) {
+                    promptComplete();
+                } else {
+                    promptUser();
+                }
+                break;
+            case Commands.Purge:
+                collection.removeComplete();
+                promptUser();
+                break;
         }
+
     })
 }
 
